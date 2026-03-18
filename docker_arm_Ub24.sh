a78b41a5d503609d91978a43334519b5e25e2196
N-rwal <104368706+N-rwal@users.noreply.github.com>
2026-03-18 20:31:17 +0100

    Add files via upload

---
+++ docker_arm_Ub24.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "installation: Docker on Arm Ubuntu 24.04"
+
+# Prerequisites
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg
+
+# Docker repo
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add user to docker group
+sudo usermod -aG docker $USER
+
+echo "Install done !"
